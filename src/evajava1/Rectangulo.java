8470c6ac23bcd0604a0a3228b4c325c524f7e072
badenk4271 <ayavarav@gmail.com>
2017-10-31 19:45:10 -0300

    Se creo clase rectangulo y se delararon variables, se aplico Scanner para que corriera y se ejecutara

--- src/evajava1/Rectangulo.java
+++ src/evajava1/Rectangulo.java
@@ -4,11 +4,28 @@
  * and open the template in the editor.
  */
 package evajava1;
+import java.util.Scanner;// (paso 1)
+
 
 /**
  *
  * @author LAB01
  */
-public class Rectangulo {
+public class Rectangulo 
+{
+  public static void main(String[] args) //(paso 2 declarar variables)
+  {
+      float area, base, altura;
+      Scanner sc=new Scanner(System.in);//Paso 3
+      System.out.println("Ingrese la base del rectangulo");//paso4
+      base = sc.nextFloat();//leo y guardo los datos de entrada 
+      System.out.println("Ingrese la altura del rectangulo");
+      altura = sc.nextFloat();
+      
+      area= base*altura; //paso 5
+      System.out.println("El  area del rectangulo es:"+area+" mts2");
+  } 
+    
+    
     
 }
